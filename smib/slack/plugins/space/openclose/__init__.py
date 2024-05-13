e24d9383ecd36a629680f6030e0b9b024fd55c2e
Sam Cork <sam57719@gmail.com>
2024-05-13 22:48:02 +0100

    Update app home layout and functionality in SMIB plugin
    Added a new section to the app_home layout, introducing the current status of "So Make It" and its respective styles. Updated button styles based on space state. Extended functionality in the get_app_home function and 'space_open'/'space_closed' actions, implementing view republishing upon state changes for accurate display.

--- smib/slack/plugins/space/openclose/__init__.py
+++ smib/slack/plugins/space/openclose/__init__.py
@@ -8,37 +8,59 @@ from smib.common.utils import http_bolt_response, is_json_encodable
 from smib.slack.custom_app import CustomApp
 from slack_sdk import WebClient
 from slack_sdk.models.views import View
-from slack_sdk.models.blocks import ActionsBlock, PlainTextObject, HeaderBlock, ButtonElement
+from slack_sdk.models.blocks import ActionsBlock, PlainTextObject, HeaderBlock, ButtonElement, SectionBlock, \
+    MarkdownTextObject
 
 from .models import Space
 
 from smib.common.config import SPACE_OPEN_ANNOUNCE_CHANNEL_ID
 from smib.slack.db import database
+from inspect import unwrap
 
 app: CustomApp = inject("SlackApp")
 
 
+@database()
 def get_app_home():
+    blocks = []
+
+    space = Space.single()
+    #
+    # func = unwrap(get_space_state)
+
+    # Header
     header_text = PlainTextObject(text="Welcome to S.M.I.B.", emoji=True)
     header_block = HeaderBlock(text=header_text)
+    blocks.append(header_block)
+
+    # State Text
+    state_text = MarkdownTextObject(
+        text=":warning: Unable to determine if So Make It is open or closed! :warning:" if space.open is None else
+             f"So Make It is currently *{'open' if space.open else 'closed'}*!",
+                                    )
+    state_text_block = SectionBlock(text=state_text)
+    blocks.append(state_text_block)
 
     # Buttons
     open_button = ButtonElement(
         text=PlainTextObject(text="Space Open", emoji=True),
         value="open",
-        action_id="space_open"
+        action_id="space_open",
+        style="primary" if space.open is None or not space.open else None
     )
 
     closed_button = ButtonElement(
         text=PlainTextObject(text="Space Closed", emoji=True),
         value="closed",
-        action_id="space_closed"
+        action_id="space_closed",
+        style="danger" if space.open is None or space.open else None
     )
 
     # Action block with buttons
     action_block = ActionsBlock(elements=[open_button, closed_button])
+    blocks.append(action_block)
 
-    result = View(type="home", blocks=[header_block, action_block])
+    result = View(type="home", blocks=blocks)
 
     return result
 
@@ -51,8 +73,7 @@ def app_home_opened(client: WebClient, event: dict):
 @app.action('space_open')
 @app.event('http_put_space_open')
 @database()
-def space_open(say, context, ack):
-
+def space_open(say, context, ack, client):
     ack()
     context['logger'].debug("Space Open!")
     say(text='Space Open!', channel=SPACE_OPEN_ANNOUNCE_CHANNEL_ID)
@@ -60,12 +81,16 @@ def space_open(say, context, ack):
     space: Space = Space.single()
     space.set_open()
 
+    # If we have a user_id, this means that this is from the app_home button
+    user_id = context.get('user_id', None)
+    if user_id is not None:
+        client.views_publish(user_id=user_id, view=get_app_home())
+
 
 @app.action('space_closed')
 @app.event('http_put_space_closed')
 @database()
-def space_closed(say, context, ack):
-
+def space_closed(say, context, ack, client):
     ack()
     context['logger'].debug("Space Closed!")
 
@@ -73,6 +98,11 @@ def space_closed(say, context, ack):
 
     space: Space = Space.single()
     space.set_closed()
+
+    # If we have a user_id, this means that this is from the app_home button
+    user_id = context.get('user_id', None)
+    if user_id is not None:
+        client.views_publish(user_id=user_id, view=get_app_home())
 
 
 @app.event("http_get_space_state")
