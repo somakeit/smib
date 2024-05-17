7d5c5bfb2ddadadc798db78d022f92bca81ec307
Stephen Jefferson <stephen@sjefferson.co.uk>
2024-05-17 18:44:02 +0100

    Docstrings and tidying

--- smibhid/lib/LCD1602.py
+++ smibhid/lib/LCD1602.py
@@ -1,11 +1,11 @@
 ## Originally copied from https://files.waveshare.com/upload/d/db/LCD1602_I2C_Module_code.zip
 
 # -*- coding: utf-8 -*-
-import time
-from machine import Pin,I2C
+from time import sleep
+from machine import I2C
 from ulogging import uLogger
 
-#Device I2C Arress
+#Device I2C address
 LCD_ADDRESS   =  (0x7c>>1)
 
 LCD_CLEARDISPLAY = 0x01
@@ -44,79 +44,85 @@ LCD_2LINE = 0x08
 LCD_1LINE = 0x00
 LCD_5x8DOTS = 0x00
 
-
 class LCD1602:
-  def __init__(self, log_level: int, i2c_id: int,  i2c_sda: int, i2c_scl: int, col: int, row: int):
-    self.log = uLogger("LCD1602", log_level)
-    self.log.info("Init LCD1602 display driver")
-    self._row = row
-    self._col = col
+	"""Drive for the LCD1602 16x2 character LED display"""
+	def __init__(self, log_level: int, i2c_id: int,  i2c_sda: int, i2c_scl: int, col: int, row: int) -> None:
+		"""Configure and connect to display via I2C, throw error on connection issue."""
+		self.log = uLogger("LCD1602", log_level)
+		self.log.info("Init LCD1602 display driver")
+		self._row = row
+		self._col = col
 
-    try:
-      self.LCD1602_I2C = I2C(i2c_id, sda = i2c_sda, scl = i2c_scl ,freq = 400000)
-      self._showfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS
-      self.begin(self._row,self._col)
-    except BaseException:
-      self.log.error("Error connecting to LCD display on I2C bus. Check I2C pins and ID and that correct module (I2C address) is connected.")
-      raise
-        
-  def command(self,cmd):
-    self.LCD1602_I2C.writeto_mem(LCD_ADDRESS, 0x80, chr(cmd))
+		try:
+			self.LCD1602_I2C = I2C(i2c_id, sda = i2c_sda, scl = i2c_scl ,freq = 400000)
+			self._showfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS
+			self._begin(self._row)
+		except BaseException:
+			self.log.error("Error connecting to LCD display on I2C bus. Check I2C pins and ID and that correct module (I2C address) is connected.")
+			raise
+				
+	def _command(self, cmd: int) -> None:
+		"""Execute a command against the display driver. Refer to command constants."""
+		self.LCD1602_I2C.writeto_mem(LCD_ADDRESS, 0x80, chr(cmd))
 
-  def write(self,data):
-    self.LCD1602_I2C.writeto_mem(LCD_ADDRESS, 0x40, chr(data))
+	def _write(self, data: int) -> None:
+		self.LCD1602_I2C.writeto_mem(LCD_ADDRESS, 0x40, chr(data))
 
-  def setCursor(self,col,row):
-    if(row == 0):
-      col|=0x80
-    else:
-      col|=0xc0
-    self.LCD1602_I2C.writeto(LCD_ADDRESS, bytearray([0x80,col]))
+	def setCursor(self, col: int, row: int) -> None:
+		"""Position the cursor ahead of writing a character or string."""
+		if(row == 0):
+			col|=0x80
+		else:
+			col|=0xc0
+		self.LCD1602_I2C.writeto(LCD_ADDRESS, bytearray([0x80, col]))
 
-  def clear(self):
-    self.command(LCD_CLEARDISPLAY)
-    time.sleep(0.002)
-  def printout(self,arg):
-    if(isinstance(arg,int)):
-      arg=str(arg)
+	def clear(self) -> None:
+		"""Clear the entire screen."""
+		self._command(LCD_CLEARDISPLAY)
+		sleep(0.002)
 
-    for x in bytearray(arg,'utf-8'):
-      self.write(x)
+	def printout(self, arg: str) -> None:
+		"""Print a string to the cursor position."""
+		if(isinstance(arg, int)):
+			arg=str(arg)
 
+		for x in bytearray(arg, 'utf-8'):
+			self._write(x)
 
-  def display(self):
-    self._showcontrol |= LCD_DISPLAYON 
-    self.command(LCD_DISPLAYCONTROL | self._showcontrol)
-
+	def _display(self) -> None:
+		"""Turn on display"""
+		self._showcontrol |= LCD_DISPLAYON 
+		self._command(LCD_DISPLAYCONTROL | self._showcontrol)
  
-  def begin(self,cols,lines):
-    if (lines > 1):
-        self._showfunction |= LCD_2LINE 
-     
-    self._numlines = lines 
-    self._currline = 0 
+	def _begin(self, lines: int) -> None:
+		"""Configure and set initial display output"""
+		if (lines > 1):
+				self._showfunction |= LCD_2LINE 
+		 
+		self._numlines = lines 
+		self._currline = 0 
 
-    time.sleep(0.05)
+		sleep(0.05)
 
-    # Send function set command sequence
-    self.command(LCD_FUNCTIONSET | self._showfunction)
-    #delayMicroseconds(4500);  # wait more than 4.1ms
-    time.sleep(0.005)
-    # second try
-    self.command(LCD_FUNCTIONSET | self._showfunction)
-    #delayMicroseconds(150);
-    time.sleep(0.005)
-    # third go
-    self.command(LCD_FUNCTIONSET | self._showfunction)
-    # finally, set # lines, font size, etc.
-    self.command(LCD_FUNCTIONSET | self._showfunction)
-    # turn the display on with no cursor or blinking default
-    self._showcontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF 
-    self.display()
-    # clear it off
-    self.clear()
-    # Initialize to default text direction (for romance languages)
-    self._showmode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT 
-    # set the entry mode
-    self.command(LCD_ENTRYMODESET | self._showmode)
-    # backlight init
+		# Send function set command sequence
+		self._command(LCD_FUNCTIONSET | self._showfunction)
+		#delayMicroseconds(4500);  # wait more than 4.1ms
+		sleep(0.005)
+		# second try
+		self._command(LCD_FUNCTIONSET | self._showfunction)
+		#delayMicroseconds(150);
+		sleep(0.005)
+		# third go
+		self._command(LCD_FUNCTIONSET | self._showfunction)
+		# finally, set # lines, font size, etc.
+		self._command(LCD_FUNCTIONSET | self._showfunction)
+		# turn the display on with no cursor or blinking default
+		self._showcontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF 
+		self._display()
+		# clear it off
+		self.clear()
+		# Initialize to default text direction (for romance languages)
+		self._showmode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT 
+		# set the entry mode
+		self._command(LCD_ENTRYMODESET | self._showmode)
+		# backlight init
--- smibhid/lib/display.py
+++ smibhid/lib/display.py
@@ -10,7 +10,13 @@ def check_enabled(method):
         return wrapper
 
 class Display:
+    """
+    Display management for SMIBHID to drive displays via driver classes abstracting display constraints from the messaging required.
+    Provides functions to clear display and print top or bottom line text.
+    Currently supports 2x16 character LCD display.
+    """
     def __init__(self, log_level: int) -> None:
+        """Connect to display using configu file values for I2C"""
         self.log = uLogger("Display", log_level)
         self.log.info("Init display")
         self.enabled = True
@@ -22,23 +28,28 @@ class Display:
     
     @check_enabled
     def clear(self) -> None:
+        """Clear entire screen"""
         self.lcd.clear()
     
     def _text_to_line(self, text: str) -> str:
+        """Internal function to ensure line fits the screen and no previous line text is present for short strings."""
         text = text[:16]
         text = "{:<16}".format(text)
         return text
 
     @check_enabled
     def print_top_line(self, text: str) -> None:
+        """Print up to 16 characters on the top line."""
         self.lcd.setCursor(0, 0)
         self.lcd.printout(self._text_to_line(text))
     
     @check_enabled
     def print_bottom_line(self, text: str) -> None:
+        """Print up to 16 characters on the bottom line."""
         self.lcd.setCursor(0, 1)
         self.lcd.printout(self._text_to_line(text))
 
     @check_enabled
     def print_space_state(self, state: str) -> None:
+        """Abstraction for space state formatting and placement."""
         self.print_bottom_line(f"Space: {state}")
