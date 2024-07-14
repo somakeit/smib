5008d718995f99589e92e79050f0eefc4ac41c4f
Stephen Jefferson <stephen@sjefferson.co.uk>
2024-07-14 18:36:07 +0100

    feat: Update main.py file to include a docstring and fix formatting issue in startup() call

--- smibhid/main.py
+++ smibhid/main.py
@@ -1,3 +1,7 @@
+"""
+Main file for the SMIB HID pico code. This file is responsible for starting the
+HID class and running.
+"""
 # Built against Pico W Micropython firmware v1.22.2 https://micropython.org/download/RPI_PICO_W/
 
 from lib.hid import HID
