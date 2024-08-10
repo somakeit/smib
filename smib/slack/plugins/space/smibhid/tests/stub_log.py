e72fc7c9367da33989194e216ff5ce1f217981a2
Stephen Jefferson <stephen@sjefferson.co.uk>
2024-08-10 13:09:34 +0100

    Added valid json header that breaks SMIB response

--- smib/slack/plugins/space/smibhid/tests/stub_log.py
+++ smib/slack/plugins/space/smibhid/tests/stub_log.py
@@ -15,9 +15,10 @@ def main():
         {"event": {"button_name": "Space Closed", "button_id": "space_closed"}, "type": "button_press",
          "timestamp": int(time.time())}
     ]
-    headers = {'device-hostname': "smibhid-dummy"}
+    headers = {"Content-Type": "application/json", 'device-hostname': "smibhid-dummy"}
     url = f"http://localhost/smib/event/smibhid_ui_log"
     print(f"url: {url}")
+    print(f"headers: {headers}")
     print(f"data: {data}")
     print(f"JSON data: {json.dumps(data)}")
     response = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
