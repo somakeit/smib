e3af7e920107debfa52166ac5aaac6ddb1f43987
Sam Cork <sam57719@gmail.com>
2024-10-13 20:59:18 +0100

    Fresh start - barebones smib package

--- src/smib/config.py
+++ src/smib/config.py
@@ -2,5 +2,3 @@ from decouple import config
 
 SLACK_BOT_TOKEN: str = config("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN: str = config("SLACK_APP_TOKEN")
-
-PLUGIN_DIRECTORY: str = "plugins"
--- src/smib/utils.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-import smib
-
-
-def get_root_path():
-    return Path(smib.__file__).resolve().parent.parent
