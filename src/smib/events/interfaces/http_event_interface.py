78222d3ca9a4ec94c5cda375ed00f7b6ade92935
Sam Cork <sam57719@gmail.com>
2025-02-25 00:12:49 +0000

    Add utility function to retrieve Slack Bolt reserved args
    Introduce `get_reserved_args` to return reserved argument names from `AsyncArgs` annotations. This can help ensure proper handling of arguments within Slack Bolt integrations.

--- src/smib/events/interfaces/http_event_interface.py
+++ src/smib/events/interfaces/http_event_interface.py
@@ -1,7 +1,9 @@
 from slack_bolt.app.async_app import AsyncApp
+from slack_bolt.kwargs_injection.async_args import AsyncArgs
 
 from smib.events.handlers.http_event_handler import HttpEventHandler
 from smib.events.services.http_event_service import HttpEventService
+
 
 class HttpEventInterface:
     def __init__(self, bolt_app: AsyncApp, handler: HttpEventHandler, service: HttpEventService):
@@ -9,3 +11,6 @@ class HttpEventInterface:
         self.handler = handler
         self.service = service
 
+
+def get_reserved_args():
+    return set(AsyncArgs.__annotations__.keys())
