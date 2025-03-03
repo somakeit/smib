fb71c7e0f2263488ad524087f1a56e93e6f5ecec
Sam Cork <sam57719@gmail.com>
2025-03-03 22:12:28 +0000

    Rename 'space' plugin to 'spacestate' and refine models.
    Updated the plugin structure by renaming 'space' to 'spacestate' for better clarity. Improved the `SpaceState` model by annotating fields with detailed descriptions for consistency and readability.

--- src/plugins/spacestate/models.py
+++ src/plugins/spacestate/models.py
@@ -7,8 +7,8 @@ from pydantic import BaseModel, Field, ConfigDict, with_config
 
 class SpaceState(Document):
     # Redefine mongodb default 'id' field to be excluded from the schema
-    id: Annotated[Optional[PydanticObjectId], Field(default=None, description="MongoDB document ObjectID", exclude=True)]
-    open: bool | None = None
+    id: Annotated[PydanticObjectId | None, Field(default=None, description="MongoDB document ObjectID", exclude=True)]
+    open: Annotated[bool | None, Field(default=None, description="Whether the space is open")]
 
     class Settings:
         name = "space_state"
