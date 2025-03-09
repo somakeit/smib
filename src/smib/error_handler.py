d0466ef3569336c427592315492b7cb5f9a2c7b5
Sam Cork <sam57719@gmail.com>
2025-03-09 15:52:10 +0000

    Handle HTTP exceptions in BoltRequestMode.HTTP
    Added specific handling for HTTP exceptions when Bolt operates in HTTP mode. Integrated FastAPI's `http_exception_handler` to process exceptions and return an appropriate `HttpBoltResponse`.

--- src/smib/error_handler.py
+++ src/smib/error_handler.py
@@ -1,24 +1,29 @@
 import logging
 import pprint
-from enum import StrEnum
-from http import HTTPStatus
 from logging import Logger
 
+from fastapi import HTTPException
+from fastapi.exception_handlers import http_exception_handler
 from slack_bolt import BoltResponse
 from slack_bolt.error import BoltUnhandledRequestError
 from slack_bolt.listener.async_listener_error_handler import AsyncDefaultListenerErrorHandler
 from slack_bolt.request.async_request import AsyncBoltRequest
 
 from smib.events.handlers import BoltRequestMode
+from smib.events.responses.http_bolt_response import HttpBoltResponse
 
 default_error_handler_logger = logging.getLogger(AsyncDefaultListenerErrorHandler.__name__)
 default_error_handler = AsyncDefaultListenerErrorHandler(default_error_handler_logger)
 
-async def error_handler(error: Exception, request: AsyncBoltRequest, body: dict, response: BoltResponse, logger: Logger):
+async def error_handler(error: Exception | HTTPException, request: AsyncBoltRequest, body: dict, response: BoltResponse, logger: Logger):
 
     match request.mode:
         case BoltRequestMode.SOCKET_MODE if isinstance(error, BoltUnhandledRequestError):
             return await default_error_handler.handle(error, request, response)
+        case BoltRequestMode.HTTP if isinstance(error, HTTPException):
+            # noinspection PyTypeChecker
+            response = await http_exception_handler(None, error)
+            return HttpBoltResponse(status=response.status_code, body=response.body, headers=dict(response.headers), fastapi_response=response)
         case _:
             logger.info(f'Request mode: {request.mode}')
             logger.warning(pprint.pformat(body, sort_dicts=False), exc_info=error)
