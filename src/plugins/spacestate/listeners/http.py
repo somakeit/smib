21fc3a01b087cb74b7c0feeb0e6cae0e5e158cf7
Sam Cork <sam57719@gmail.com>
2025-03-16 11:51:06 +0000

    Refactor `get_space_state` to remove unused `say` parameter
    The `say` parameter in the `get_space_state` function was unused and has been removed for clarity and better code maintainability. Additionally, import statements were rearranged for improved readability and organization.

--- src/plugins/spacestate/listeners/http.py
+++ src/plugins/spacestate/listeners/http.py
@@ -3,8 +3,10 @@ from http import HTTPStatus
 from slack_bolt.context.say.async_say import AsyncSay
 
 from smib.events.interfaces.http_event_interface import HttpEventInterface
+from ..common import set_space_state_in_db, send_space_open_announcement, send_space_closed_announcement, \
+    get_space_state_from_db
 from ..models import SpaceState, SpaceStateEnum, SpaceStateOpen
-from ..common import set_space_state_in_db, send_space_open_announcement, send_space_closed_announcement, get_space_state_from_db
+
 
 def register(http: HttpEventInterface):
 
@@ -25,7 +27,7 @@ def register(http: HttpEventInterface):
 
 
     @http.get("/space/state", response_model=SpaceState)
-    async def get_space_state(say: AsyncSay):
+    async def get_space_state():
         """ Get the space state """
         space_state = await get_space_state_from_db()
         return space_state
