17fc31db6b810272049f45a5483c71b8a4d1a20f
Sam Cork <sam57719@gmail.com>
2025-04-06 22:38:36 +0100

    Add logging for active Slack websocket connections
    Introduced a handler to log the number of active Slack websocket connections when a "hello" message is received. This helps in monitoring and debugging connection status effectively.

--- src/smib/events/services/slack_event_service.py
+++ src/smib/events/services/slack_event_service.py
@@ -1,6 +1,8 @@
+import json
 import logging
 from logging import Logger
 
+from aiohttp import WSMessage
 from slack_bolt.adapter.socket_mode.aiohttp import AsyncSocketModeHandler
 from slack_bolt.app.async_app import AsyncApp
 
@@ -18,10 +20,17 @@ class SlackEventService:
     @lazy_property
     def service(self) -> AsyncSocketModeHandler:
         service = AsyncSocketModeHandler(self.bolt_app, app_token=SLACK_APP_TOKEN)
+        service.client.on_message_listeners.append(self.log_number_of_connections)
         return service
+
+    async def log_number_of_connections(self, raw_message: WSMessage):
+        json_message = json.loads(raw_message.data)
+        if json_message.get("type") == "hello":
+            self.logger.info(f"Number of active slack websocket connections: {json_message.get('num_connections')}")
 
     async def start(self):
         await self.service.start_async()
 
     async def stop(self):
         await self.service.close_async()
+
