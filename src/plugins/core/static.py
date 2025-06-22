8433e3545fdfebfff28be4dd6fed3854d54b571e
Sam Cork <sam57719@gmail.com>
2025-06-22 10:25:39 +0100

    Add custom API Docs plugin and refine static file handling
    - Introduced `api_docs.py` to override default API documentation endpoints (`/docs` and `/redoc`) with custom behavior.
    - Updated static file handler in `static.py` to improve directory creation logging and fix typo in comments.

--- src/plugins/core/static.py
+++ src/plugins/core/static.py
@@ -18,15 +18,15 @@ def register(http: HttpEventInterface):
     resolved_static_directory_path: Path = STATIC_FILES_DIRECTORY.resolve()
     logger.info(f"Resolved static files directory to {resolved_static_directory_path}")
     if not resolved_static_directory_path.exists():
-        logger.info(f"Creating static files directory at {resolved_static_directory_path}")
+        logger.debug(f"Creating static files directory at {resolved_static_directory_path}")
         resolved_static_directory_path.mkdir(parents=True)
 
     logger.info(f"Mounting static files directory at /static")
 
     # TODO (eventually): Replace this with normal StaticFiles mounting
-    #  That method oesnt work due to https://github.com/fastapi/fastapi/issues/10180
+    #  That method doesn't currently work due to https://github.com/fastapi/fastapi/issues/10180
     #  More info: https://github.com/fastapi/fastapi/discussions/9070
-    @http.get("/static/{rest_of_path:path}", response_class=Response, include_in_schema=False)
+    @http.get("/static/{rest_of_path:path}", include_in_schema=False)
     async def static_files(rest_of_path: str):
         file_path = resolved_static_directory_path / rest_of_path
         if not file_path.exists():
