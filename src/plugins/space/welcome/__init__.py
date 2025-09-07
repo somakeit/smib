5805a40479fd00c729730de65c04335e8c806af5
Sam Cork <sam57719@gmail.com>
2025-09-07 11:42:53 +0100

    Refactor "Welcome" plugin to include `/welcome` command and improve event handling
    - Introduce `/welcome` Slack command to send welcome messages on-demand.
    - Extract `send_welcome_message` as a reusable async function.
    - Add `pprint` for debugging team join events.
    - Update `team_join` event to acknowledge and use the refactored message function.

--- src/plugins/space/welcome/__init__.py
+++ src/plugins/space/welcome/__init__.py
@@ -5,12 +5,11 @@ import json
 import re
 from functools import lru_cache
 from pathlib import Path
+from pprint import pprint
 
 from slack_bolt.app.async_app import AsyncApp
 from slack_bolt.context.ack.async_ack import AsyncAck
 from slack_bolt.context.say.async_say import AsyncSay
-
-from smib.events.interfaces.http_event_interface import HttpEventInterface
 
 
 @lru_cache
@@ -18,10 +17,22 @@ def get_welcome_message_blocks() -> dict:
     with (Path(__file__).parent / "welcome.json").open("r", encoding="utf-8") as f:
         return json.load(f)
 
-def register(slack: AsyncApp, http: HttpEventInterface):
+async def send_welcome_message(say: AsyncSay, user: str) -> None:
+    await say(blocks=get_welcome_message_blocks(), channel=user, text="Welcome to SoMakeIt!")
+
+
+def register(slack: AsyncApp):
     @slack.event("team_join")
-    async def welcome(event: dict, say: AsyncSay):
-        await say(blocks=get_welcome_message_blocks(), channel=event["user"]["id"])
+    async def welcome(event: dict, say: AsyncSay, ack: AsyncAck):
+        await ack()
+        pprint(event)
+        await send_welcome_message(say, event["user"]["id"])
+
+    @slack.command('/welcome')
+    async def welcome_command(ack: AsyncAck, say: AsyncSay, command: dict, client):
+        await ack()
+        await client.chat_postEphemeral(channel=command["channel_id"], user=command["user_id"], text="Check messages from @SMIB...")
+        await send_welcome_message(say, command["user_id"])
 
     @slack.action(re.compile('welcome_message_url_.*'))
     async def handle_welcome_message_url_clicks(ack: AsyncAck):
