a90f7bccf2d081a00d388e6161d5574894a799f6
Sam Cork <sam57719@gmail.com>
2025-09-07 12:57:54 +0100

    Update "Welcome" plugin to improve event handling and message structure
    - Enhance `/welcome` description in the manifest.
    - Add `team_join` event to event subscriptions.
    - Redesign welcome message with a table for better readability.
    - Introduce logging for new user join events.
    - Refactor `get_welcome_message_blocks` with a cache size limit.
    - Fix `/welcome` command to restrict non-DM usage and improve feedback.

--- src/plugins/space/welcome/__init__.py
+++ src/plugins/space/welcome/__init__.py
@@ -2,17 +2,19 @@ __display_name__ = "Welcome"
 __description__ = "Welcomes new users"
 
 import json
+import logging
 import re
 from functools import lru_cache
 from pathlib import Path
-from pprint import pprint
 
 from slack_bolt.app.async_app import AsyncApp
 from slack_bolt.context.ack.async_ack import AsyncAck
 from slack_bolt.context.say.async_say import AsyncSay
+from slack_sdk.web.async_client import AsyncWebClient
 
+logger = logging.getLogger("Welcome Plugin")
 
-@lru_cache
+@lru_cache(maxsize=1)
 def get_welcome_message_blocks() -> dict:
     with (Path(__file__).parent / "welcome.json").open("r", encoding="utf-8") as f:
         return json.load(f)
@@ -25,13 +27,24 @@ def register(slack: AsyncApp):
     @slack.event("team_join")
     async def welcome(event: dict, say: AsyncSay, ack: AsyncAck):
         await ack()
-        pprint(event)
-        await send_welcome_message(say, event["user"]["id"])
+
+        user_id = event["user"]["id"]
+        username = event["user"]["name"]
+
+        logger.debug(f"New user joined the slack workspace: {username}")
+        await send_welcome_message(say, user_id)
 
     @slack.command('/welcome')
-    async def welcome_command(ack: AsyncAck, say: AsyncSay, command: dict, client):
+    async def welcome_command(ack: AsyncAck, say: AsyncSay, command: dict, client: AsyncWebClient, context: dict):
         await ack()
-        await client.chat_postEphemeral(channel=command["channel_id"], user=command["user_id"], text="Check messages from @SMIB...")
+
+        if command["channel_name"] != "directmessage":
+            await client.chat_postEphemeral(
+                channel=command["channel_id"],
+                user=command["user_id"],
+                text=f"Check your messages from <@{context["bot_user_id"]}>!"
+            )
+
         await send_welcome_message(say, command["user_id"])
 
     @slack.action(re.compile('welcome_message_url_.*'))
