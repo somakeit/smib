f79b2322836d56c7d44776d50ee0d2ab28e2a45a
Sam Cork <sam57719@gmail.com>
2026-01-25 11:01:55 +0000

    refactor(logging): remove redundant error details in configuration initialization logs

--- src/smib/config/utils.py
+++ src/smib/config/utils.py
@@ -1,5 +1,4 @@
 import logging as logging_lib
-from pprint import pformat
 from typing import Optional
 
 from pydantic import ValidationError
@@ -64,8 +63,6 @@ def init_plugin_settings(settings_cls: type[BaseSettings_T], logger: logging_lib
     settings = init_settings(settings_cls, errors)
     if settings is None:
         logger.debug(f"Failed to initialise {settings_cls.__name__}: {errors}")
-        for _, error in errors:
-            logger.debug(f"{pformat(error.errors())}")
         logger.error(format_validation_errors(errors))
         raise AssertionError('Invalid configuration') from errors[0][1]
 
